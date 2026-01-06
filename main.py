f5940db6c3d5df7b437b3cbf5054bc683a23ef29
figmo <akafigmo@proton.me>
2026-01-05 19:34:28 -0600

    cleanup

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def main():
 
 
 def make_pretty(arr):
-    print(arr.shape)
     # arr[:, :, 3] = 255
 
 
@@ -33,14 +32,11 @@ def make_pretty(arr):
         # Get a random point on x, y for red channel
         point_x = int(random.uniform(0, 500))
         point_y = int(random.uniform(0, 500))
-        print(f"Point: x={point_x}, y={point_y}")
 
         # Add the random rectangle to the image at the point
         # slice the arrayi
-        print(rect.shape)
         dx = rect.shape[0]
         dy = rect.shape[1]
-        print(f"dx={dx}, dy={dy}")
         slice_x = min(point_x+dx, 500)
         slice_y = min(point_y+dy, 500)
 
@@ -48,14 +44,12 @@ def make_pretty(arr):
         # The rectangle can only be as big as
         # the minimum of 200-point_x and dx
         rect = rect[:min(dx, 500-point_x), :min(dy, 500-point_y)]
-        print(f"Truncated Rectangle: h={rect.shape[0]}, w={rect.shape[1]}")
 
         color = int(random.uniform(0, 3))
         
         arr[point_x:slice_x, point_y:slice_y, color] = rect
         arr[point_x:slice_x, point_y:slice_y, 3] = 255
 
-    print(arr[0, 0, 0])
     return arr
 
 def make_random_rectangle():
@@ -64,7 +58,6 @@ def make_random_rectangle():
     w = int(random.uniform(1, max_width))
     h = int(random.uniform(1, max_height))
     rectangle = np.zeros((h, w), dtype=np.uint8)
-    print(f"Rectangle: w={w}, h={h}")
     color = random.uniform(50, 255)
     rectangle[:,:]=color
     return rectangle
